a2c5d6b78113d851fd07b71681d560c7841c9c45
SaiKatkar009 <138797296+SaiKatkar009@users.noreply.github.com>
2026-01-21 16:18:52 +0530

    Update and rename Assignment1 .py to TASK 1 .py

--- TASK 1 .py
+++ TASK 1 .py
@@ -7,10 +7,4 @@ print("Substraction:",a-b)
 print("Multiplication:",a*b)
 print("divison:",a/b)
 
-#TASK 2 
-a = input("Enter your first name:")
-b = input("Enter your last name :")
-c = b+("!")
-
-print("Hello,",a+(" ") +c,("Welcome to python program. "))
 
